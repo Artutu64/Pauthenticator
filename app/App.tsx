60ccdcb239a8795e46b3600b762902bcb0511cd2
gabygabz <gaby.brenot@gmail.com>
2025-02-27 18:18:12 +0100

    Dev mercredi aprem

--- app/App.tsx
+++ app/App.tsx
@@ -1,39 +1,65 @@
-import React, { useState } from 'react';
-import { View, Modal, FlatList } from 'react-native';
-import Header from './src/components/Header';
-import FloatingButton from './src/components/Bouton_scan';
-import styles from './src/styles/body_styles';
-import QRCodeScannerScreen from './src/components/QRCodeScanner';
-import Card from './src/components/Card';
+import React, { useState, useEffect } from 'react';
+import { View, Modal, FlatList, Text, Button } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera';
 
+// INCLUSION DU STYLE DU BACKGROUND
+import styles from './src/styles/body_styles';
+
+// INCLUSION DES COMPONENTS
+import Header from './src/components/Header';
+import Card from './src/components/Card';
+import FloatingButton from './src/components/Bouton_scan';
+
+
+// FONCTION PRINCIPALE
 const App = () => {
+
   const [scannedData, setScannedData] = useState<string[]>([]);
   const [isScannerOpen, setIsScannerOpen] = useState(false);
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device = useCameraDevice("back", {
+    physicalDevices: [
+      "ultra-wide-angle-camera",
+      "wide-angle-camera",
+      "telephoto-camera",
+    ],
+  });
 
-  // Fonction appelée lorsqu'un QR Code est scanné
-  const handleScanSuccess = (event: { data: string }) => {
-    setScannedData([...scannedData, event.data]); // Ajoute la donnée au tableau
-    setIsScannerOpen(false); // Ferme le scanner après un scan
-  };
+  //
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes) => {
+      console.log(`canned ${codes.length} codes!`)
+    }
+  })
 
+  // CONTENUE AFFICHÉ
   return (
     <View style={styles.container}>
+      
+      {/* Component Header avec titre et timer */}
       <Header />
-
-      {/* Liste des QR Codes scannés */}
-      <FlatList
-        data={scannedData}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => <Card content={item} />}
-      />
 
       {/* Scanner en modal */}
       <Modal visible={isScannerOpen} animationType="slide">
-        <QRCodeScannerScreen onScanSuccess={handleScanSuccess} />
+        {device ? (
+            <Camera style={{
+              marginTop: 50,
+              width: "80%", // Réduit la largeur à 80% de l'écran
+              height: "60%", // Réduit la hauteur à 60% de l'écran
+              alignSelf: "center", // Centre horizontalement
+              borderRadius: 20, // Coins arrondis
+              overflow: "hidden", // Empêche la caméra de dépasser
+              backgroundColor: "white", // Fond blanc si la caméra ne remplit pas tout
+            }} device={device} codeScanner={codeScanner} isActive={true}/>
+        ) : (
+            <Text>Aucun appareil photo disponible</Text>
+        )}
       </Modal>
 
-      {/* Bouton flottant pour ouvrir le scanner */}
+      {/* Component Bouton flottant pour ouvrir le scanner */}
       <FloatingButton onPress={() => setIsScannerOpen(true)} />
+    
     </View>
   );
 };
