b45756a690e5a8238a6c0e4a06d6356f8111f542
DEPRETER-Remi <depreterremi@gmail.com>
2025-02-28 14:19:56 +0100

    pls marche

--- web/backend/tests/totp.js
+++ web/backend/tests/totp.js
@@ -1,7 +1,6 @@
-
 const { getTOTPfromURI } = require("../src/totp/TOTP");
 
-const input = prompt("Entrez le code :");
+const input = "otpauth://totp/Pauthenticator:test@test.Te?secret=JN4USP5TJSYZAI3PPGJ7N4273D32MIRSLQIIRJSSJ4AXJCIXPC5Q&issuer=Pauthenticator&algorithm=SHA512&digits=8&period=30"
 
 let totp = getTOTPfromURI(input)
 setInterval(() => {
