13de80577a8a7c2f16a3675f4b973c9fbe3858cf
DEPRETER-Remi <depreterremi@gmail.com>
2025-03-03 17:22:19 +0100

    logo appli + home appli

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 // INCLUSION DU STYLE
 import styles from '../styles/header_styles'; 
@@ -10,6 +10,7 @@ import styles from '../styles/header_styles';
 const Header = () => {
   return (
     <View style={styles.header}>
+      <Image source={require('../../logo_blanc.png')} style={styles.logo} resizeMode="contain" />   
       <Text style={styles.title}>Pauthenticator</Text>
     </View>
   );
