9877ecb7969e1cd703777768f09e56c3236b8a69
DEPRETER-Remi <depreterremi@gmail.com>
2025-03-04 16:49:53 +0100

    dernier fix css appli

--- app/src/styles/home_screen_styles.ts
+++ app/src/styles/home_screen_styles.ts
@@ -1,17 +1,19 @@
 // FICHIER STYLE DU BACKGROUND DE LA HOMEPAGE DE L'APP
 
 import { StyleSheet } from 'react-native';
+import { Tooltip } from 'react-native-paper';
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
-    paddingTop: 95, // Décalage sous le Header pour éviter la superposition
+    paddingTop: 102, // Décalage sous le Header pour éviter la superposition
   },
   scrollView: {
     flex: 1,
   },
   cardsContainer: {
+    paddingTop : 10,
   },
 });
 
